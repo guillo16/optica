e5bdcbef071d2afbb8502c7fdc171ac1f9eebffe
guillermo varela <guillovarela16@gmail.com>
2020-05-14 16:21:10 -0300

    Views created

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,15 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :subtitle, presence: true
+  has_one_attached :photo
+  validates :content, presence: true
+  validate :photo_attach
+
+  private
+
+  def photo_attach
+    if photo.attached? == false
+      errors.add(:photo, 'Tienes que elegir una foto')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'contacts/new'
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/new'
-  get 'articles/edit'
+  get 'pages/about', 'pages#about'
   devise_for :users
   root to: 'pages#home'
+  resources :articles
+  resources :contacts, only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
