1ffc686e269d665e72650ef1eee4972917c052e6
guillermo varela <guillovarela16@gmail.com>
2020-05-17 13:01:38 -0300

    Finish app mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,14 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://opticairis.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-
   ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_APP_PASSWORD'],
+    authentication: :login,
+    enable_starttls_auto: true
   }
   # Settings specified here will take precedence over those in config/application.rb.
 
